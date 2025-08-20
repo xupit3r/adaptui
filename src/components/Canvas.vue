fec7358588d96d40da9352aa039d7b147de073ab
Joe D'Alessandro <joe@thejoeshow.net>
2025-08-20 13:31:01 -0400

    initialize

---
+++ src/components/Canvas.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { ref, onMounted, reactive } from 'vue';
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true
+  },
+  height: {
+    type: Number,
+    required: true
+  },
+  width: {
+    type: Number,
+    required: true
+  }
+});
+
+const stuff = reactive({
+  ctx: null
+})
+
+const canvas = ref<HTMLCanvasElement | null>(null);
+
+onMounted(() => {
+  stuff.ctx = canvas.value.getContext("2d");
+
+  stuff.ctx.fillStyle = "rgb(200 0 0)";
+  stuff.ctx.fillRect(10, 10, 50, 50);
+
+  stuff.ctx.fillStyle = "rgb(0 0 200 / 50%)";
+  stuff.ctx.fillRect(30, 30, 50, 50);
+});
+</script>
+
+<template>
+  <canvas :id="id" 
+          :height="height" 
+          :width="width"
+          ref="canvas"
+          ></canvas>
+</template>
+
+<style lang="css"></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Canvas from '@/components/Canvas.vue';
+</script>
+
+<template>
+  <main>
+    <Canvas id="me" height=500 width=500 />
+  </main>
+</template>
