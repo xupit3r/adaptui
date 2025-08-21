46d9bf2a8e9298805a0fdd05831f9925cac99369
Joe D'Alessandro <joe@thejoeshow.net>
2025-08-21 18:44:02 -0400

    setup infrastructure for saving

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
+import { provide } from 'vue';
+
 import Text from '@/components/inputs/Text.vue';
 import Checkbox from '@/components/inputs/Checkbox.vue';
 import Radio from '@/components/inputs/Radio.vue';
 
-import { computed } from 'vue';
 const {
   type,
   name,
@@ -45,6 +46,10 @@ const {
 });
 
 const isType = (t: string) => type === t;
+
+provide('save', () => {
+  console.log(`saving ${type} field.`)
+});
 
 </script>
 
--- src/components/inputs/Text.vue
+++ src/components/inputs/Text.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { inject } from 'vue';
+
+const save: Function | undefined = inject('save');
+</script>
 
 <template>
-  <input type="text" />
+  <input type="text" @change="save" />
 </template>
 
 <style lang="css" scoped>
